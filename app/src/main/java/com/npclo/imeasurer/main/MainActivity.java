a3da5d894755e58a09a74e9302af139af6db9087
liutsing <liutsingluo@163.com>
2017-12-15 13:50:21 +0800

    ~

--- app/src/main/java/com/npclo/imeasurer/main/MainActivity.java
+++ app/src/main/java/com/npclo/imeasurer/main/MainActivity.java
@@ -106,7 +106,7 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         SharedPreferences preferences = getSharedPreferences(getString(R.string.app_name), MODE_APPEND);
         macAddress = preferences.getString("mac_address", null);
         if (TextUtils.isEmpty(macAddress)) {
-            navView.getMenu().add(R.id.device, R.id.nav_device, 0, "连接设备(未绑定设备)").setIcon(R.drawable.ic_blueteeth_unconnected);
+            navView.getMenu().add(R.id.device, R.id.nav_device, 0, "连接智能尺").setIcon(R.drawable.ic_blueteeth_unconnected);
         } else {
             deviceName = preferences.getString("device_name", null);
             updateBlueToothState(deviceName);
@@ -126,7 +126,7 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     public void updateBlueToothState(String name) {
         //先清除蓝牙设备信息菜单项
         navView.getMenu().removeItem(R.id.nav_device);
-        navView.getMenu().add(R.id.device, R.id.nav_device, 0, "连接设备(已绑定: " + name + ")")
+        navView.getMenu().add(R.id.device, R.id.nav_device, 0, "连接智能尺(已绑定: " + name + ")")
                 .setIcon(R.drawable.ic_blueteeth_connected);
     }
 
